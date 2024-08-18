1be2664316d2a655e06eb3b9476f72b020811f8a
Saikoushik-cognavi <ananthojus.k@cognavi.com>
2024-08-18 21:12:31 +0530

    project added using different APIS

---
+++ index.js
@@ -0,0 +1,226 @@
+const countryList = {
+
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+};
+
+
+let BASE_URL = 'https://v6.exchangerate-api.com/v6/550316e2c7df1f69ba9df741/latest/'
+
+const scountries = document.querySelectorAll('.select_countries')
+
+console.log(scountries[0])
+
+
+
+for(select_btn of scountries)
+{
+    for(countryCode in countryList)
+    {
+        const new_country = document.createElement('option')
+        new_country.innerText = countryCode
+        new_country.value = countryCode
+        if(select_btn.name === "from" && countryCode === "USD")
+        {
+            new_country.selected = "selected"
+        }
+        else if(select_btn.name === "to" && countryCode === "INR")
+        {
+            new_country.selected = "selected"
+        }
+        select_btn.appendChild(new_country)
+    }
+    select_btn.addEventListener('change',(e)=>{
+        const countryCode = e.target.value
+        const country = countryList[countryCode]
+        const img = e.target.parentElement.querySelector("img")
+        img.src = `https://flagsapi.com/${country}/flat/64.png`
+    })
+}
+
+const ExchangeRate = async () => {
+
+    let amount = document.getElementById('amount').value
+    if(amount === "" || amount < 1)
+    {
+        amount = 1
+        amount.value = "1"
+    }
+    let fromcountry = document.querySelector('.from .select_countries').value
+    let tocountry = document.querySelector('.to .select_countries').value
+    let result = document.querySelector('.msg')
+    const url = BASE_URL + fromcountry
+    const response = await fetch(url)
+    const body = await response.json()
+    console.log(body["conversion_rates"][tocountry])
+    console.log(amount)
+    result.innerText = `${amount}${fromcountry}=${amount * body["conversion_rates"][tocountry]}${tocountry}`
+}
+
+
+document.querySelector('.btn').addEventListener('click',async (e)=>{
+    ExchangeRate()
+});
+
+
+
+
+
+
